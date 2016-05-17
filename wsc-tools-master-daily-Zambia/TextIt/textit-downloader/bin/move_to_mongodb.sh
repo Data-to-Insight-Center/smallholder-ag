508f54796f930436ec3b681be4150a835fc14965
nbisht7 <nbisht2890@gmail.com>
2016-05-16 23:00:15 -0400

    deleted folders
    deleted

--- wsc-tools-master-daily-Zambia/TextIt/textit-downloader/bin/move_to_mongodb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-for f in /u/smallag/textit-data/Zambia/daily/`date +%Y-%m-%d`/*.json; do
-/usr/bin/mongoimport --db WSC --collection zambia`date +%Y%m%d`daily --username smallag --password Supriya2890 $f
-done
--- wsc-tools-master-daily-Zambia/TextIt/textit-downloader/bin/start-downloader.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# set env
-bin=`dirname "$0"`
-bin=`cd "$bin"; pwd`
-. "$bin"/env.sh
-
-$JAVA $JAVA_HEAP_MAX -classpath "$TEXTIT_DOWNLOADER_CLASSPATH" \
-  edu.indiana.d2i.textit.TextItDownloader &
