8b0cc1f32c9cac4bbd5f46352077d0ceffc6cd7e
charitha306 <charitha306@gmail.com>
2016-08-05 15:25:55 -0400

    Adding scripts to check mongodb status nightly

---
+++ textit/textit-monitor/monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# check mongod
+if [ ! -f /var/run/mongodb/mongod.pid ] || ! ps -p `cat /var/run/mongodb/mongod.pid` > /dev/null ; then
+  echo "Mongo main database is down"
+  echo "Mongo main database is down" | mail -s "Smallholder-Ag MongoDB Alert" charmadu@umail.iu.edu
+fi
+# check mongod_raw
+if [ ! -f /var/run/mongodb/mongod_raw.pid ] || ! ps -p `cat /var/run/mongodb/mongod_raw.pid` > /dev/null ; then
+  echo "Mongo raw database is down"
+  echo "Mongo raw database is down" | mail -s "Smallholder-Ag MongoDB Alert" charmadu@umail.iu.edu
+fi
+
