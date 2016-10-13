e86c000bb9f43cb9abf5d7af95775e37aaa2ba91
charitha306 <charitha306@gmail.com>
2016-10-13 14:04:40 -0400

    Adding example email address to monitoring script

--- textit/textit-monitor/monitor.sh
+++ textit/textit-monitor/monitor.sh
@@ -2,11 +2,11 @@
 # check mongod
 if [ ! -f /var/run/mongodb/mongod.pid ] || ! ps -p `cat /var/run/mongodb/mongod.pid` > /dev/null ; then
   echo "Mongo main database is down"
-  echo "Mongo main database is down" | mail -s "Smallholder-Ag MongoDB Alert" charmadu@umail.iu.edu
+  echo "Mongo main database is down" | mail -s "Smallholder-Ag MongoDB Alert" example@umail.iu.edu
 fi
 # check mongod_raw
 if [ ! -f /var/run/mongodb/mongod_raw.pid ] || ! ps -p `cat /var/run/mongodb/mongod_raw.pid` > /dev/null ; then
   echo "Mongo raw database is down"
-  echo "Mongo raw database is down" | mail -s "Smallholder-Ag MongoDB Alert" charmadu@umail.iu.edu
+  echo "Mongo raw database is down" | mail -s "Smallholder-Ag MongoDB Alert" example@umail.iu.edu
 fi
 
