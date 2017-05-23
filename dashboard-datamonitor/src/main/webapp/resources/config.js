df0fdf4ccee6d66f8bd5d4c0c9af219936ebd7c5
rkunalan <kunarath@iu.edu>
2017-05-23 15:49:45 -0400

    user access authentication added

---
+++ dashboard-datamonitor/src/main/webapp/resources/config.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+gOptions = {
+  // This can have nested stuff, arrays, etc.
+  uname: 'YOUR_USERNAME',
+  pwd: 'YOUR_PASSWORD',
+  zam_api_url: './api/zambia',
+  ken_api_url: './api/kenya',
+  enabled: true
+}
